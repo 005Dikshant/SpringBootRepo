6f05cf644c6a22dfda19fcee700fb4cecf944bd3
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2025-01-03 16:40:26 -0500

    Logging inside the Spring boot using SL4J logger class

--- javaspring/src/main/java/com/javaprojects/javaspring/controller/DepartmentController.java
+++ javaspring/src/main/java/com/javaprojects/javaspring/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.javaprojects.javaspring.controller;
 
 import com.javaprojects.javaspring.entity.Department;
 import com.javaprojects.javaspring.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,16 +12,21 @@ import java.util.List;
 @RestController
 public class DepartmentController {
 
+    private final Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+
+
     @Autowired
     DepartmentService departmentService;
 
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
+        logger.info("Inside the saveDepartment");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> getListAllDeparments(){
+        logger.info("Inside the getListAllDeparments");
         return departmentService.getListAllDeparments();
     }
 

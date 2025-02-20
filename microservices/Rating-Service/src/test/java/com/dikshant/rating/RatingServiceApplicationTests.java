829903aa21b41a80ba193f026a29e199a70377df
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2025-02-20 11:06:45 -0500

    Setting up the Rating service

---
+++ microservices/Rating-Service/src/test/java/com/dikshant/rating/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikshant.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

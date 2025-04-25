5def0d5285efd09d2dedc2631a8a15afd0485bdd
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2025-04-25 12:09:05 -0400

    Setting up basic of config server microservice

---
+++ microservices/ConfigServer/src/test/java/com/dikshant/configserver/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikshant.configserver.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
